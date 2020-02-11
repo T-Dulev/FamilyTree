e6bb10fc8975118e6922bc71484ef6344fbde2e6
T-Dulev <dulev@abv.bg>
2020-02-11 17:41:01 +0200

    FamilyTree Нов проект за четене и визуализация на родословно дърво от GedCom файл

---
+++ FamilyTreeForm.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace FamilyTree
+{
+    partial class FamilyTreeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listPersons = new System.Windows.Forms.ListView();
+            this.ID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Person = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Parrents = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Children = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.buttonLoadTree = new System.Windows.Forms.Button();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel = new System.Windows.Forms.ToolStripLabel();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listPersons
+            // 
+            this.listPersons.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.ID,
+            this.Person,
+            this.Parrents,
+            this.Children});
+            this.listPersons.GridLines = true;
+            this.listPersons.HideSelection = false;
+            this.listPersons.Location = new System.Drawing.Point(12, 109);
+            this.listPersons.Name = "listPersons";
+            this.listPersons.Size = new System.Drawing.Size(420, 277);
+            this.listPersons.TabIndex = 0;
+            this.listPersons.UseCompatibleStateImageBehavior = false;
+            this.listPersons.View = System.Windows.Forms.View.Details;
+            // 
+            // ID
+            // 
+            this.ID.Text = "ID";
+            this.ID.Width = 43;
+            // 
+            // Person
+            // 
+            this.Person.Text = "Person";
+            this.Person.Width = 122;
+            // 
+            // Parrents
+            // 
+            this.Parrents.Text = "Parrents";
+            this.Parrents.Width = 88;
+            // 
+            // Children
+            // 
+            this.Children.Text = "Children";
+            this.Children.Width = 88;
+            // 
+            // buttonLoadTree
+            // 
+            this.buttonLoadTree.Location = new System.Drawing.Point(13, 13);
+            this.buttonLoadTree.Name = "buttonLoadTree";
+            this.buttonLoadTree.Size = new System.Drawing.Size(75, 23);
+            this.buttonLoadTree.TabIndex = 1;
+            this.buttonLoadTree.Text = "Load tree";
+            this.buttonLoadTree.UseVisualStyleBackColor = true;
+            this.buttonLoadTree.Click += new System.EventHandler(this.buttonLoadTree_Click);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 425);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
+            this.toolStrip1.Size = new System.Drawing.Size(800, 25);
+            this.toolStrip1.TabIndex = 2;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripLabel
+            // 
+            this.toolStripLabel.Name = "toolStripLabel";
+            this.toolStripLabel.Size = new System.Drawing.Size(0, 22);
+            // 
+            // FamilyTreeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.buttonLoadTree);
+            this.Controls.Add(this.listPersons);
+            this.Name = "FamilyTreeForm";
+            this.Text = "FamilyTree";
+            this.Load += new System.EventHandler(this.FamilyTreeForm_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listPersons;
+        private System.Windows.Forms.ColumnHeader Person;
+        private System.Windows.Forms.ColumnHeader ID;
+        private System.Windows.Forms.ColumnHeader Parrents;
+        private System.Windows.Forms.ColumnHeader Children;
+        private System.Windows.Forms.Button buttonLoadTree;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel;
+    }
+}
+
---
+++ FamilyTreeForm.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GeneGenie.Gedcom.Parser;
+using GeneGenie.Gedcom;
+
+namespace FamilyTree
+{
+    public partial class FamilyTreeForm : Form
+    {
+        public FamilyTreeForm()
+        {
+            InitializeComponent();
+        }
+
+        private void FamilyTreeForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonLoadTree_Click(object sender, EventArgs e)
+        {
+            var gedcomData = GedcomRecordReader.CreateReader("J:\\Family\\dulev2020.ged");
+            toolStripLabel.Text = "Loaded " + gedcomData.Database.Count + " records";
+
+            var db = new GedcomDatabase();
+
+            var individual = gedcomData.Database.Individuals.Select(f => f.Names).ToList();
+
+            var ID = 0;
+            foreach (var item in individual)
+            {
+                foreach (var itemPerson in item)
+                {
+                    ID++;
+                    listPersons.Items.Add(new ListViewItem(new string[4] { ID.ToString(), itemPerson.Name, "", "" }));
+                }
+            }
+
+
+            //foreach (var item in gedcomData.Database.Individuals)
+            //{
+            //    foreach (var itemPerson in item.Names)
+            //    {
+            //        listPersons.Items.Add(new ListViewItem(new string[4] { itemPerson.Name, itemPerson.Surname, "", "" }));
+
+            //    }
+
+            //}
+        }
+    }
+}
